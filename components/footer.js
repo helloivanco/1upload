bbb2c0a23c9df4d25f23916ff676ad589d8aea59
Paid_co <hellopaidco@gmail.com>
2021-08-15 20:18:49 +0100

    first commit

--- components/footer.js
+++ components/footer.js
@@ -3,15 +3,15 @@ export default function Footer() {
     <footer className='flex items-center justify-center w-full h-24 text-gray-500'>
       ♾️
       <a
-        className='flex items-center justify-center ml-1 mr-2 hover:underline'
+        className='flex items-center justify-center ml-1 mr-2 hover:underline hover:text-gray-400'
         href='https://web3.storage'
         target='_blank'
         rel='noopener noreferrer'>
         Web3.Storage
       </a>
-      |
+      <span className='text-gray-700'>|</span>
       <a
-        className='flex items-center justify-center ml-2 hover:underline'
+        className='flex items-center justify-center ml-2 hover:underline hover:text-gray-400'
         href='https://github.com/hellopaidco/1upload'
         target='_blank'
         rel='noopener noreferrer'>
